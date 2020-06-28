1230c285cc8ccf3f8e62f8c2981abb5cb17d529e
Trung Thu Nguyen <ngut37@vse.cz>
2020-06-29 01:15:52 +0200

    feat getLesson by id

---
+++ src/lesson/lesson.service.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Lesson } from './lesson.entity';
+import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+@Injectable()
+export class LessonService {
+  constructor(
+    @InjectRepository(Lesson) private lessonRepository: Repository<Lesson>,
+  ) {}
+
+  async createLesson(name, startDate, endDate): Promise<Lesson> {
+    const lesson = this.lessonRepository.create({
+      id: uuid(),
+      name,
+      startDate,
+      endDate,
+    });
+    return this.lessonRepository.save(lesson);
+  }
+
+  async getLesson(id): Promise<Lesson> {
+    return this.lessonRepository.findOne({ id });
+  }
+}
